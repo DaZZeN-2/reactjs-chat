95d653661dd8b5e6e180e9939a0f1740b6049d21
Maksim Kolyada <qwezmaksim21@gmail.com>
2023-04-15 20:29:09 +0300

    #

--- src/Pages/ChatsPage.jsx
+++ src/Pages/ChatsPage.jsx
@@ -4,7 +4,7 @@ const ChatsPages = (props) => {
   return (
     <div style={{ height: "100vh" }}>
       <PrettyChatWindow
-        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID}
+        projectId='8953563f-429a-4c54-bca2-14fb18dab13d'
         username={props.user.username}
         secret={props.user.secret}
         style={{ height: "100%" }}
